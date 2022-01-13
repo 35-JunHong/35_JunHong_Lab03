8834995c33ff8a10e5ccd30b4b9d813341568434
JunHong <neojunhong2003@gmail.com>
2022-01-13 10:42:41 +0800

    Initial commit

---
+++ Assets/Scripts/BasketMovementScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketMovementScript : MonoBehaviour
+{
+    public float speed;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float minXPos = -10f;
+        float maxXPos = 10f;
+        float xMovement = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        if (transform.position.x + xMovement > minXPos && transform.position.x + xMovement < maxXPos)
+        {
+            transform.Translate(xMovement, 0f, 0f);
+        }
+        //float horizontalInput = Input.GetAxis("Horizontal");
+        //transform.position = transform.position + new Vector3(horizontalInput * speed * Time.deltaTime, 0, 0);
+    }
+}
